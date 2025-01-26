67004750c192b70060cead0e6a4859d8a406bf71
fazleenik <fazlee.awscloud@gmail.com>
2025-01-26 11:27:06 +0800

    vv
    v

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    hmr: {
+      timeout: 0 // Disable HMR timeout
+    }
   },
   plugins: [
     react(),
